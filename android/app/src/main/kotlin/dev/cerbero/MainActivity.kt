4e73a32a1c8b97f21e0ec0896a56b75fa95f4e51
cerberodev <hi@cerbero.dev>
2021-10-28 19:36:01 -0500

    Initial Project

---
+++ android/app/src/main/kotlin/dev/cerbero/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.cerbero
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:userapp/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
